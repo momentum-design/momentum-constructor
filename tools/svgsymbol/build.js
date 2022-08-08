e40bb283a763a943b87a411707a1a516cf6781f0
Xiaole Tao (xiaolet) <xiaolet@cisco.com>
2022-08-09 00:24:36 +0800

    refactor svgsymbol builder.

--- tools/svgsymbol/build.js
+++ tools/svgsymbol/build.js
@@ -1,134 +1,67 @@
 /**
+ * This is example to show you how to build svg symbol with svgSymbolBuilder.
  * @Copyright 2022 Cisco Systems Inc. All rights reserved.
  * @Description
  * @Author Xiaole Tao <xiaolet@cisco.com>
- * @Date 2022/8/3
- * @Update 2022/8/5
+ * @Date 2022/8/8
+ * @Update 2022/8/8
  */
 
-let fs = require("fs"),
-    path = require("path"),
-    cheerio = require("cheerio"),
-    beautify = require("xml-beautifier"),
-    baseUrl = __dirname,
-    packagePath = require.resolve("momentum-abstract"),
-    conf = {
-        svgSource: path.resolve(packagePath, "../icon"),
-        targetFolder: path.resolve(baseUrl, "dist"),
-        targetSvgFile: path.resolve(baseUrl, "dist/icons.svg"),
-        targetJsFile: path.resolve(baseUrl, "dist/icons.js"),
-        targetIndexFile: path.resolve(baseUrl, "dist/index.html"),
-        blackListTags: ["symbol", "title", "desc", "use", "script"],
-        viewBox: {}
-    };
+const svgBuilder = require("./svgSymbolBuilder"),
+    path = require("path");
 
-let svgBuilder = {
-    initialize: function () {
-        if (!fs.existsSync(conf.targetFolder)) fs.mkdirSync(conf.targetFolder);
-        conf.svgFileList = svgBuilder.getSvgfileList(conf.svgSource);
-    },
-
-    getSvgfileList: function (iconPath) {
-        return fs
-            .readdirSync(iconPath)
-            .filter((name) => fs.statSync(path.resolve(iconPath, name)).isFile() && name.match(/^(.*)\.svg$/))
-            .map((name) => name.replace(/\.svg$/, ""));
-    },
-
-    isEmptyTag: function ($tag) {
-        return JSON.stringify($tag.attr()) === "{}" && !$tag.children().length;
-    },
-
-    fetchSymbol: function (type, name, refer) {
-        let file = path.resolve(conf.svgSource, name + ".svg"),
-            fileContent = fs.readFileSync(file, { encoding: "utf8" }),
-            $ = cheerio.load(fileContent, { normalizeWhitespace: false, xmlMode: true }),
-            svg = $("svg"),
-            children = svg.children(),
-            symbolContent = [];
-
-        conf.viewBox[name] = svg.attr("viewBox") || `0 0 ${svg.attr("width") || 32} ${svg.attr("height") || 32}`;
-        svg.find("g,circle,ellipse,image,line,path,pattern,polygon,polyline,rect,text").removeAttr("id");
-        symbolContent.push('<symbol id="' + refer + '" viewBox="' + conf.viewBox[name] + '">');
-
-        for (let i = 0; i < children.length; i++) {
-            let child = children.eq(i);
-            if (!conf.blackListTags.includes(children.get(i).tagName) && !svgBuilder.isEmptyTag(child)) {
-                symbolContent.push($.html(child));
-            }
-        }
-        symbolContent.push("</symbol>");
-        return beautify(symbolContent.join(""));
-    },
-
-    buildSymbol: function () {
-        let svgContent = ['<svg xmlns="http://www.w3.org/2000/svg">'];
-        conf.svgFileList.forEach(function (name) {
-            svgContent.push(svgBuilder.fetchSymbol("svgfile", name, name));
-        });
-        svgContent.push("</svg>");
-        return svgContent;
-    },
-
-    buildIndexPage: function () {
-        let indexContent = [
-            "<!DOCTYPE html>",
-            '<html lang="en"><head><meta charset="UTF-8"><title>Lib Icons Index</title>',
-            "<style>",
-            "svg{display:block;margin:4px auto 0;}",
-            "symbol{transition-property:fill;transition-duration:.5s;fill:BlueViolet;}",
-            ".icon-pack{border: 1px solid #666;border-radius: 10px;width: 100px;height: 80px;display: inline-block;overflow: hidden;margin: 5px;background: #a1a1a1cc;}",
-            ".title {font: 9pt/9pt arial;text-align: center;background: #CCC;padding: 3px 0 0;height: 30px;}",
-            'body{background:url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 8 8"><path fill="rgb(192,192,192)" d="M0 0L4 0L4 8L8 8L8 4L0 4Z"/></svg>\') repeat}',
-            "</style>",
-            "</head><body>"
-        ];
-        conf.svgFileList.forEach(function (name) {
-            let refer = name;
-            indexContent.push('<div class="icon-pack">');
-            indexContent.push('<div class="title">' + refer + "</div>");
-            indexContent.push('<svg style="width: 32px; height: 32px;"><use xlink:href="#' + refer + '"/></svg>');
-            indexContent.push("</div>");
-        });
-        indexContent.push("</body></html>");
-        indexContent.push('<script src="icons.js"></script>');
-        indexContent.push("<script>");
-        indexContent.push(
-            'document.body.insertAdjacentHTML("afterBegin", "<div style=\\"height:0;position:absolute;\\">" + window.namespace.svgSymbol + \'</div>\');'
-        );
-        indexContent.push('var cl=["Yellow","GreenYellow","Lime","DarkSlateGray","BlueViolet"];');
-        indexContent.push("window.setInterval(function(){cl.push(cl.shift());document.styleSheets[0].cssRules[1].style.fill=cl[0];},15000);");
-        indexContent.push('document.body.addEventListener("click",function(e){if(e.target.localName==="input"){e.target.select()}});');
-        indexContent.push("</script>");
-        return indexContent;
-    },
-
-    buildJSFile: function (svgContent) {
-        return [
-            "window.namespace = window.namespace || {};",
-            "window.namespace.svgSymbol = [",
-            svgContent
-                .join("\n")
-                .split("\n")
-                .map((line) => "'" + line + "'")
-                .join(",\n"),
-            "].join('\\n');"
-        ].join("\n");
-    },
-
-    doBuild: function () {
-        svgBuilder.initialize();
-        let svgContent = svgBuilder.buildSymbol();
-
-        console.log("Saving builded file to ", conf.targetSvgFile);
-        fs.writeFileSync(conf.targetSvgFile, svgContent.join("\n"), { encoding: "utf8" });
-        console.log("Saving JS file to ", conf.targetJsFile);
-        fs.writeFileSync(conf.targetJsFile, svgBuilder.buildJSFile(svgContent), { encoding: "utf8" });
-        console.log("Saving index file to ", conf.targetIndexFile);
-        fs.writeFileSync(conf.targetIndexFile, svgBuilder.buildIndexPage().join("\n"), { encoding: "utf8" });
-
-        console.log("Build success.");
+// Example1: Direct dobuild with parameters
+svgBuilder.doBuild({
+    namespace: "icons1",
+    targetFolder: path.resolve(__dirname, "dist"),
+    targetSvgFile: "icons1.svg",
+    targetJsFile: "icons1.js",
+    targetIndexFile: "icon1.html",
+    svgList: {
+        accessibility_bold: "v2_accessibility_bold_32",
+        admin_bold: "v2_admin_bold_32",
+        alert_bold: "v2_notification_bold_32",
+        annotate_bold: "v2_annotate_bold_32",
+        announcement_bold: "v2_feedback_bold_32",
+        appearance_bold: "v2_appearance_bold_32",
+        applause_bold: "v2_emoji_applaud_bold_32",
+        application_regular: "v2_share_application",
+        applications_bold: "v2_applications_bold_32",
+        apps_bold: "v2_apps_bold_32",
+        apps_filled: "v2_apps_filled_32"
     }
-};
+});
 
+// Example2: Setup and dobuild
+svgBuilder.setup({
+    namespace: "icons2",
+    targetFolder: path.resolve(__dirname, "dist"),
+    targetSvgFile: "icons2.svg",
+    targetJsFile: "icons2.js",
+    targetIndexFile: "icon2.html",
+    svgList: [
+        "pause_bold",
+        "pen_bold",
+        "phone_bold",
+        "pin-muted_bold",
+        "pin_bold",
+        "play_bold",
+        "plus_bold",
+        "poll_bold",
+        "pop-in_bold",
+        "pop-out_bold",
+        "private-circle_bold",
+        "q-a_bold"
+    ]
+});
 svgBuilder.doBuild();
+
+// Example3: No svgList, dobuild all icons, only JS symbol file
+svgBuilder.doBuild({
+    namespace: "icons3",
+    targetFolder: path.resolve(__dirname, "dist"),
+    targetSvgFile: null,
+    targetJsFile: "icons3.js",
+    targetIndexFile: "icon3.html",
+    svgList: null
+});
---
+++ tools/svgsymbol/svgSymbolBuilder.js
@@ -0,0 +1,191 @@
+/**
+ * @Copyright 2022 Cisco Systems Inc. All rights reserved.
+ * @Description
+ * @Author Xiaole Tao <xiaolet@cisco.com>
+ * @Date 2022/8/3
+ * @Update 2022/8/5
+ */
+
+let fs = require("fs"),
+    path = require("path"),
+    cheerio = require("cheerio"),
+    beautify = require("xml-beautifier"),
+    baseUrl = __dirname,
+    packagePath = require.resolve("momentum-abstract"),
+    conf = {
+        svgSource: path.resolve(packagePath, "../icon"),
+        blackListTags: ["symbol", "title", "desc", "use", "script"],
+        isMissedFile: false
+    };
+
+const svgSymbolBuilder = new Function();
+
+Object.assign(svgSymbolBuilder, {
+    /**
+     * @param {*} config - {
+     *    namespace: string, default is "cisco"
+     *    taretFolder: string, default is "[root]/tools/svgsymbol/dist"
+     *    targetSvgFile: string, default is "icons.svg"
+     *    targetJsFile: string, default is "icons.js"
+     *    targetIndexFile: string, default is "index.html"
+     *    svgList: array | object, default is all icons.
+     *            ["iconName", "iconName", ...] or
+     *            {"iconName": "icon id", "iconName": "icon id", ...} or
+     *            leave it null to build all icons
+     * }
+     */
+    setup: function (config) {
+        config = config || {};
+        conf.namespace = config.namespace || "cisco";
+        conf.targetFolder = config.targetFolder || path.resolve(baseUrl, "dist");
+        conf.targetSvgFile = config.targetSvgFile === null ? null : path.resolve(conf.targetFolder, config.targetSvgFile || "icons.svg");
+        conf.targetJsFile = config.targetJsFile === null ? null : path.resolve(conf.targetFolder, config.targetJsFile || "icons.js");
+        conf.targetIndexFile = config.targetIndexFile === null ? null : path.resolve(conf.targetFolder, config.targetIndexFile || "index.html");
+
+        conf.svgList = config.svgList || {};
+
+        if (!fs.existsSync(conf.targetFolder)) fs.mkdirSync(conf.targetFolder);
+        conf.svgFileList = svgSymbolBuilder.getSvgfileList();
+    },
+
+    getSvgfileList: function () {
+        let fileList,
+            idList = [];
+        if (Array.isArray(conf.svgList) && conf.svgList.length > 0) {
+            fileList = conf.svgList;
+        } else if (!Array.isArray(conf.svgList) && typeof conf.svgList === "object" && Object.getOwnPropertyNames(conf.svgList).length > 0) {
+            fileList = Object.keys(conf.svgList);
+            idList = Object.values(conf.svgList);
+        } else {
+            fileList = fs
+                .readdirSync(conf.svgSource)
+                .filter((name) => fs.statSync(path.resolve(conf.svgSource, name)).isFile() && name.match(/^(.*)\.svg$/))
+                .map((name) => name.replace(/\.svg$/, ""));
+        }
+        return fileList.map((name, index) => {
+            return { icon: name, id: idList[index] || name };
+        });
+    },
+
+    isEmptyTag: function ($tag) {
+        return JSON.stringify($tag.attr()) === "{}" && !$tag.children().length;
+    },
+
+    fetchSymbol: function (name, id) {
+        let file = path.resolve(conf.svgSource, name + ".svg"),
+            fileContent;
+
+        if (fs.existsSync(file) && fs.statSync(file).isFile()) {
+            fileContent = fs.readFileSync(file, { encoding: "utf8" });
+        } else {
+            console.error("Missing file: " + file);
+            conf.isMissedFile = true;
+            return "";
+        }
+
+        let $ = cheerio.load(fileContent, { normalizeWhitespace: false, xmlMode: true }),
+            svg = $("svg"),
+            children = svg.children(),
+            symbolContent = [],
+            viewbox = svg.attr("viewBox") || `0 0 ${svg.attr("width") || 32} ${svg.attr("height") || 32}`;
+        svg.find("g,circle,ellipse,image,line,path,pattern,polygon,polyline,rect,text").removeAttr("id");
+        symbolContent.push('<symbol id="' + id + '" viewBox="' + viewbox + '">');
+
+        for (let i = 0; i < children.length; i++) {
+            let child = children.eq(i);
+            if (!conf.blackListTags.includes(children.get(i).tagName) && !svgSymbolBuilder.isEmptyTag(child)) {
+                symbolContent.push($.html(child));
+            }
+        }
+        symbolContent.push("</symbol>");
+        return beautify(symbolContent.join(""));
+    },
+
+    buildSymbol: function () {
+        let svgContent = ['<svg xmlns="http://www.w3.org/2000/svg">'];
+        conf.svgFileList.forEach(function (item) {
+            svgContent.push(svgSymbolBuilder.fetchSymbol(item.icon, item.id));
+        });
+        svgContent.push("</svg>");
+        return svgContent;
+    },
+
+    buildIndexPage: function () {
+        let indexContent = [
+            "<!DOCTYPE html>",
+            '<html lang="en"><head><meta charset="UTF-8"><title>Lib Icons Index</title>',
+            "<style>",
+            "svg{display:block;margin:4px auto 0;}",
+            "symbol{transition-property:fill;transition-duration:.5s;fill:BlueViolet;}",
+            ".icon-pack{border: 1px solid #666;border-radius: 10px;width: 100px;height: 80px;display: inline-block;overflow: hidden;margin: 5px;background: #a1a1a1cc;}",
+            ".title {font: 9pt/9pt arial;text-align: center;background: #CCC;padding: 3px 0 0;height: 30px;}",
+            'body{background:url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 8 8"><path fill="rgb(192,192,192)" d="M0 0L4 0L4 8L8 8L8 4L0 4Z"/></svg>\') repeat}',
+            "</style>",
+            "</head><body>"
+        ];
+        conf.svgFileList.forEach(function (item) {
+            let refer = item.id;
+            indexContent.push('<div class="icon-pack">');
+            indexContent.push('<div class="title">' + refer + "</div>");
+            indexContent.push('<svg style="width: 32px; height: 32px;"><use xlink:href="#' + refer + '"/></svg>');
+            indexContent.push("</div>");
+        });
+        indexContent.push("</body></html>");
+        indexContent.push('<script src="' + conf.targetJsFile + '"></script>');
+        indexContent.push("<script>");
+        indexContent.push(
+            'document.body.insertAdjacentHTML("afterBegin", "<div style=\\"width:0;height:0;position:absolute;right:0;\\">" + window.' +
+                conf.namespace +
+                ".svgSymbol + '</div>');"
+        );
+        indexContent.push('var cl=["Yellow","GreenYellow","Lime","DarkSlateGray","BlueViolet"];');
+        indexContent.push("window.setInterval(function(){cl.push(cl.shift());document.styleSheets[0].cssRules[1].style.fill=cl[0];},15000);");
+        indexContent.push("</script>");
+        return indexContent;
+    },
+
+    buildJSFile: function (svgContent) {
+        return [
+            "window." + conf.namespace + " = window." + conf.namespace + " || {};",
+            "window." + conf.namespace + ".svgSymbol = [",
+            svgContent
+                .join("\n")
+                .split("\n")
+                .map((line) => "'" + line + "'")
+                .join(",\n"),
+            "].join('\\n');"
+        ].join("\n");
+    },
+
+    doBuild: function (config) {
+        if (!conf.targetFolder || typeof config === "object") {
+            svgSymbolBuilder.setup(config);
+        }
+
+        let svgContent = svgSymbolBuilder.buildSymbol();
+
+        if (conf.targetSvgFile) {
+            console.log("Saving builded file to ", conf.targetSvgFile);
+            fs.writeFileSync(conf.targetSvgFile, svgContent.join("\n"), { encoding: "utf8" });
+        }
+        if (conf.targetJsFile) {
+            console.log("Saving JS file to ", conf.targetJsFile);
+            fs.writeFileSync(conf.targetJsFile, svgSymbolBuilder.buildJSFile(svgContent), { encoding: "utf8" });
+        }
+        if (conf.targetIndexFile) {
+            console.log("Saving index file to ", conf.targetIndexFile);
+            fs.writeFileSync(conf.targetIndexFile, svgSymbolBuilder.buildIndexPage().join("\n"), { encoding: "utf8" });
+        }
+
+        if (conf.isMissedFile) {
+            console.error("Build finished, but some files are not found. Please check the parameter: svgList.");
+            process.exit(1);
+        }
+        console.log("Build success.");
+    }
+});
+
+module.exports = {
+    setup: svgSymbolBuilder.setup,
+    doBuild: svgSymbolBuilder.doBuild
+};
