737b01d723dd40edf7710d5c4369bfe54132f46b
Xiaole Tao (xiaolet) <xiaolet@cisco.com>
2022-08-23 23:13:59 +0800

    updated pages

--- svgsymbol/build.js
+++ svgsymbol/build.js
@@ -1,3 +1,16 @@
-const svgBuilder = require("momentum-constructor-svgsymbol");
+/**
+ * This is example to show you how to build svg symbol with svgSymbolBuilder.
+ * @Copyright 2022 Cisco Systems Inc. All rights reserved.
+ * @Description
+ * @Author Xiaole Tao <xiaolet@cisco.com>
+ * @Date 2022/8/23
+ * @Update 2022/8/23
+ */
 
-console.log(111);
+const svgBuilder = require("momentum-constructor-svgsymbol"),
+    path = require("path");
+
+svgBuilder.doBuild({ targetFolder: path.resolve(__dirname), targetJsFile: "icons.js", targetIndexFile: "icons.html", svgList: { icon: "all" } });
+svgBuilder.doBuild({ targetFolder: path.resolve(__dirname), targetJsFile: "color.js", targetIndexFile: "color.html", svgList: { "icon-colored": "all" } });
+svgBuilder.doBuild({ targetFolder: path.resolve(__dirname), targetJsFile: "brand.js", targetIndexFile: "brand.html", svgList: { "icon-brand": "all" } });
+svgBuilder.doBuild({ targetFolder: path.resolve(__dirname), targetJsFile: "illus.js", targetIndexFile: "illus.html", svgList: { illustration: "all" } });
