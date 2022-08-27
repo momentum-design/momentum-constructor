85d97cb190aea1a1c5f91aa3525ed3050b48c2e7
Xiaole Tao (xiaolet) <xiaolet@cisco.com>
2022-08-27 23:48:21 +0800

    refactor, add test, add template

--- tools/svgsymbol/test/svgSymbolBuilder.test.js
+++ tools/svgsymbol/test/svgSymbolBuilder.test.js
@@ -1,8 +1,16 @@
 const svgBuilder = require("../svgSymbolBuilder");
 
-describe("Test interface",()=>{
+describe("Test svgSymbolBuilder", () => {
+    test("Is interface exist", () => {
+        expect(typeof svgBuilder.doBuild).toBe("function");
+    });
 
-    test('Is interface exist', () => {
-        expect(typeof svgBuilder.doBuild === "function").toBeTruthy();
+    test("Default Parameter", () => {
+        let conf = svgBuilder._conf;
+        svgBuilder.doBuild();
+        expect(conf.namespace).toBe("cisco");
+        expect(conf.svgList.icon).toBe("all");
+        expect(conf.svgList.local).toBe("all");
+        expect(conf.svgList.illustration).toBe(undefined);
     });
 });
